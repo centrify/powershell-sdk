39a7ee42353e8ca12ab710d5d833694f901c6947
Fab Viguier <badrogh@gmail.com>
2021-07-23 13:56:33 +0100

    Adding support to update private key

---
+++ Centrify.Platform.PowerShell/Lib/Rotate-VaultSshKey.ps1
@@ -0,0 +1,112 @@
+###########################################################################################
+# Centrify Platform PowerShell module
+#
+# Author   : Fabrice Viguier
+# Contact  : support AT centrify.com
+# Release  : 21/01/2016
+# Copyright: (c) 2016 Centrify Corporation. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+#            You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+#            distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#            See the License for the specific language governing permissions and limitations under the License.
+###########################################################################################
+
+<#
+.SYNOPSIS
+This Cmdlet supports ability to rotate SSH Private Key for a given VaultSshKey.
+
+.DESCRIPTION
+This Cmdlet supports ability to rotate SSH Private Key for a given VaultSshKey.
+
+.PARAMETER VaultSshKey
+Mandatory VaultSshKey.
+
+.INPUTS
+[VaultSshKey]
+
+.OUTPUTS
+
+.EXAMPLE
+C:\PS>  Rotate-VaultSshKey -VaultSshKey (Get-VaultSshKey -Name "root@server123") 
+
+#>
+function global:Rotate-VaultSshKey
+{
+	param
+	(
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[System.Object]$VaultAccount		
+	)
+	
+	# Debug preference
+	if ($PSCmdlet.MyInvocation.BoundParameters["Debug"].IsPresent) 
+	{
+		# Debug continue without waiting for confirmation
+		$DebugPreference = "Continue"
+	}
+	else 
+	{
+		# Debug message are turned off
+		$DebugPreference = "SilentlyContinue"
+	}
+	
+	try
+	{	
+		# Get current connection to the Centrify Platform
+		$PlatformConnection = Centrify.Platform.PowerShell.Core.GetPlatformConnection
+
+		# Setup variable for query
+		$Uri = ("https://{0}/ServerManage/RotatePassword" -f $PlatformConnection.PodFqdn)
+		$ContentType = "application/json" 
+		$Header = @{ "X-CENTRIFY-NATIVE-CLIENT" = "1"; }
+
+		# Set Json query
+		$JsonQuery = @{}
+
+		if (-not [System.String]::IsNullOrEmpty($VaultAccount))
+		{
+			if ([System.String]::IsNullOrEmpty($VaultAccount.ID))
+			{
+				Throw "Cannot get PASAccount ID from given parameter."
+			}
+			else
+			{
+				# Get PASAccount ID
+				$JsonQuery.ID = $VaultAccount.ID
+			}
+		}
+
+		# Build Json query
+		$Json = $JsonQuery | ConvertTo-Json
+
+		# Debug informations
+		Write-Debug ("Uri= {0}" -f $Uri)
+		Write-Debug ("Json= {0}" -f $Json)
+				
+		# Connect using RestAPI
+		$WebResponse = Invoke-WebRequest -UseBasicParsing -Method Post -Uri $Uri -Body $Json -ContentType $ContentType -Headers $Header -WebSession $PlatformConnection.Session
+		$WebResponseResult = $WebResponse.Content | ConvertFrom-Json
+		if ($WebResponseResult.Success)
+		{
+			# Valid Job success
+            if ($WebResponseResult.Result.Success)
+            {
+                # Return nothing
+                Exit 0
+            }
+            else
+            {
+		        # Job error
+		        Throw $WebResponseResult.Result.Reason
+            }
+		}
+		else
+		{
+			# Query error
+			Throw $WebResponseResult.Message
+		}
+	}
+	catch
+	{
+		Throw $_.Exception   
+	}
+}
