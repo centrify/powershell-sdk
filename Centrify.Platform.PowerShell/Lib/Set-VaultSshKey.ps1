d7ccbd5cc203507a3734f2af04b2446c0e03d6f7
Fab Viguier <badrogh@gmail.com>
2021-07-23 13:57:44 +0100

    Adding support to update private key

--- Centrify.Platform.PowerShell/Lib/Set-VaultSshKey.ps1
+++ Centrify.Platform.PowerShell/Lib/Set-VaultSshKey.ps1
@@ -27,21 +27,31 @@ Optional parameter to specify the private key to store in PEM format
 Optional parameter to specify the key passphrase
 
 .EXAMPLE
-C:\PS>  New-VaultSshKey -Name "root@server123" -PrivateKey C:\Users\Spandey\Desktop\sshkey@30.pem 
-This CMDlet store a new SSH key name "root@server123" to the vault
+C:\PS>  Get-VaultSshKey -Name "root@server123" | Set-VaultSshKey -Name "root@server456"
+This CMDlet change SSH key name from "root@server123" to "root@server456"
+
+.EXAMPLE
+C:\PS>  Set-VaultSshKey -Id (Get-VaultSshKey -Name "root@server123") -Description "Root key for Server123"
+This CMDlet update description for SSH key name "root@server123"
 #>
 function global:Set-VaultSshKey
 {
 	param
 	(
-		[Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = "Specify the ID of the SSH Key to update.")]
-		[System.String]$Id,
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = "Specify the SSH Key to update.")]
+		[System.String]$VaultSshKey,
         
         [Parameter(Mandatory = $false, HelpMessage = "Specify the new Name for the SSH Key to update.")]
 		[System.String]$Name,
 
-		[Parameter(Mandatory = $false, HelpMessage = "Specify the Description for the SSH Key to update.")]
-		[System.String]$Description
+		[Parameter(Mandatory = $false, HelpMessage = "Specify the path to the private key in PEM format.")]
+		[System.String]$PrivateKey,
+
+		[Parameter(Mandatory = $false, HelpMessage = "Specify the private key Description.")]
+		[System.String]$Description,
+		
+		[Parameter(Mandatory = $false, HelpMessage = "Specify the private key passphrase.")]
+		[System.String]$Passphrase
 	)
 	
 	# Debug preference
@@ -56,51 +66,112 @@ function global:Set-VaultSshKey
 		$DebugPreference = "SilentlyContinue"
 	}
 
+    if (-not [System.String]::IsNullOrEmpty($Passphrase) -and [System.String]::IsNullOrEmpty($PrivateKey))
+    {
+        # Passphrase must be update along Private key
+        Throw "Passphrase can only be update when also updating Private Key. Both parameters must be used at the same time."
+    }
+
     # Get current connection to the Centrify Platform
 	$PlatformConnection = Centrify.Platform.PowerShell.Core.GetPlatformConnection
 
 	try
 	{	
-		# Setup variable for query
-		$Uri = ("https://{0}/ServerManage/UpdateSshKey" -f $PlatformConnection.PodFqdn)
-		$ContentType = "application/json" 
-		$Header = @{ "X-CENTRIFY-NATIVE-CLIENT" = "1"; }
-
-		# Set Json query
-		$JsonQuery = @{}
-		$JsonQuery.Id 	  = $Id
-
-        if (-not [System.String]::IsNullOrEmpty($Name))
+        if ([System.String]::IsNullOrEmpty($PrivateKey))
         {
-            $JsonQuery.Name = $Name
+            # Setup variable for query
+            $Uri = ("https://{0}/ServerManage/UpdateSshKey" -f $PlatformConnection.PodFqdn)
+            $ContentType = "application/json" 
+            $Header = @{ "X-CENTRIFY-NATIVE-CLIENT" = "1"; }
+
+            # Set Json query
+            $JsonQuery = @{}
+            $JsonQuery.Id 	  = $VaultSshKey.Id
+
+            if (-not [System.String]::IsNullOrEmpty($Name))
+            {
+                $JsonQuery.Name = $Name
+            }
+            if (-not [System.String]::IsNullOrEmpty($Description))
+            {
+                $JsonQuery.Comment = $Description
+            }
+
+            # Build Json query
+            $Json = $JsonQuery | ConvertTo-Json
+
+            # Debug informations
+            Write-Debug ("Uri= {0}" -f $Uri)
+            Write-Debug ("Json= {0}" -f $Json)
+                    
+            # Connect using RestAPI
+            $WebResponse = Invoke-WebRequest -UseBasicParsing -Method Post -Uri $Uri -Body $Json -ContentType $ContentType -Headers $Header -WebSession $PlatformConnection.Session
+            $WebResponseResult = $WebResponse.Content | ConvertFrom-Json
+            if ($WebResponseResult.Success)
+            {
+                # Success return SshKey
+                return (Get-VaultSshKey -Name $Name)
+            }
+            else
+            {
+                # Query error
+                Throw $WebResponseResult.Message
+            }
+
         }
-		if (-not [System.String]::IsNullOrEmpty($Description))
+        else
         {
-            $JsonQuery.Comment = $Description
+            # In order to update Private Key a new SSH Key must be created and re-associated to accounts/profiles that where using it
+            # Set new Key values
+            $SshKey = @{}
+            # Keep values from current key unless passed along PrivateKey
+            if (-not [System.String]::IsNullOrEmpty($Name))
+            {
+                $SshKey.Name = $Name
+            }
+            else 
+            {
+                $SshKey.Name = $VaultSshKey.Name
+            }
+            if (-not [System.String]::IsNullOrEmpty($Description))
+            {
+                $SshKey.Description = $Description
+            }
+            else 
+            {
+                $SshKey.Description = $VaultSshKey.Description
+            }
+
+            # Create new SSH Key
+            if (-not [System.String]::IsNullOrEmpty($Passphrase))
+            {
+                # SSH Key has a Passphrase
+                $NewVaultSshKey = New-VaultSshKey -Name $SshKey.Name -Description $SshKey.Description -PrivateKey $PrivateKey -Passphrase $Passphrase
+            }
+            else            
+            {
+                # SSH Key has no Passphrase
+                $NewVaultSshKey = New-VaultSshKey -Name $SshKey.Name -Description $SshKey.Description -PrivateKey $PrivateKey
+            }            
+
+            # Getting list of accounts and profiles using old key
+            $SshKeyUsageList = Centrify.Platform.PowerShell.DataVault.GetSshKeyUsage($VaultSshKey.Id)
+            if ($SshKeyUsageList -ne [Void]$null)
+            {
+                $SshKeyUsageList | Where-Object { $_.Type -eq "VaultAccount" } | ForEach-Object {
+                    # Re-assigning new SSH Key to Accounts
+                    ### WIP
+
+                }
+                $SshKeyUsageList | Where-Object { $_.Type -eq "DiscoveryProfile" } | ForEach-Object {
+                    # Re-assigning new SSH Key to Profiles
+                    ### WIP
+
+                }
+            }
         }
-
-		# Build Json query
-		$Json = $JsonQuery | ConvertTo-Json
-
-		# Debug informations
-		Write-Debug ("Uri= {0}" -f $Uri)
-		Write-Debug ("Json= {0}" -f $Json)
-				
-		# Connect using RestAPI
-		$WebResponse = Invoke-WebRequest -UseBasicParsing -Method Post -Uri $Uri -Body $Json -ContentType $ContentType -Headers $Header -WebSession $PlatformConnection.Session
-		$WebResponseResult = $WebResponse.Content | ConvertFrom-Json
-		if ($WebResponseResult.Success)
-		{
-			# Success return SshKey
-			return (Get-VaultSshKey -Name $Name)
-		}
-		else
-		{
-			# Query error
-			Throw $WebResponseResult.Message
-		}
-	}
-	catch
+    }
+    catch
 	{
 		Throw $_.Exception   
 	}
